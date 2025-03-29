2b662da962d98cbe8c97a940c513f3878f82e74b
mendexco <vitormendesco.js@gmail.com>
2025-03-29 14:12:33 -0300

    feat: setting up lint and base UI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import {dirname} from "path";
-import {fileURLToPath} from "url";
-import {FlatCompat} from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
+// Existing config extended to include the import plugin and rules:
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:import/recommended'),
+  {
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: ['type', 'builtin', 'object', 'external', 'internal', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            {
+              pattern: '~/**',
+              group: 'external',
+              position: 'after'
+            }
+          ],
+          'newlines-between': 'always'
+        }
+      ]
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { FC } from 'react'
+import { VisuallyHidden } from '@react-aria/visually-hidden'
+import { type SwitchProps, useSwitch } from '@heroui/switch'
+import { useTheme } from 'next-themes'
+import { useIsSSR } from '@react-aria/ssr'
+import clsx from 'clsx'
+
+export interface ThemeSwitcherProps {
+  className?: string
+  classNames?: SwitchProps['classNames']
+}
+
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className, classNames }) => {
+  const { theme, setTheme } = useTheme()
+  const isSSR = useIsSSR()
+
+  const onChange = () => setTheme(theme === 'light' ? 'dark' : 'light')
+
+  const { Component, slots, isSelected, getBaseProps, getInputProps, getWrapperProps } = useSwitch({
+    isSelected: theme === 'light' || isSSR,
+    'aria-label': `Switch to ${theme === 'light' || isSSR ? 'dark' : 'light'} mode`,
+    onChange
+  })
+
+  return (
+    <Component
+      {...getBaseProps({
+        className: clsx('px-px transition-opacity hover:opacity-80 cursor-pointer', className, classNames?.base)
+      })}
+    >
+      <VisuallyHidden>
+        <input {...getInputProps()} />
+      </VisuallyHidden>
+      <div
+        {...getWrapperProps()}
+        className={slots.wrapper({
+          class: clsx(
+            [
+              'w-auto h-auto',
+              'bg-transparent',
+              'rounded-lg',
+              'flex items-center justify-center',
+              'group-data-[selected=true]:bg-transparent',
+              '!text-default-500',
+              'pt-px',
+              'px-0',
+              'mx-0'
+            ],
+            classNames?.wrapper
+          )
+        })}
+      >
+        {!isSelected || isSSR ? <p>light</p> : <p>dark</p>}
+      </div>
+    </Component>
+  )
+}
