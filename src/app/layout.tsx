6ed5747e5e4c4518d8616dbe01a26bc765f6e6bb
mendexco <vitormendesco.js@gmail.com>
2025-03-29 15:33:40 -0300

    feat(layout.tsx): adding vercel analytics to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@styles/globals.css'
 
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from "@vercel/analytics/react"
 import { Geist, Geist_Mono } from 'next/font/google'
 import { type ReactNode } from 'react'
 import { Metadata } from 'next'
@@ -31,6 +32,7 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>{children}</ThemeProvider>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
